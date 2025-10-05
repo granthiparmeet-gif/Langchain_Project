5dd5db3fe9fd4de783eb0a66c347d4180cac6337
granthiparmeet-gif <granthi.parmeet@gmail.com>
2025-10-05 07:14:35 +0530

    adding Runnable_with_message_history.py

--- runnable_with_message_history.py
+++ runnable_with_message_history.py
@@ -1,37 +1,41 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_core.chat_history import InMemoryChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatOpenAI()
 
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant."),
+    ("system","You are a helpful assistant"),
     ("placeholder", "{history}"),
     ("human", "{input}")
-])
+]
+)
 
-chain = prompt | llm
+chain = prompt| llm
 
-# Persistent store for histories
 store = {}
 
-def get_history(session_id:str):
+def get_history(session_id : str):
     if session_id not in store:
         store[session_id] = InMemoryChatMessageHistory()
     return store[session_id]
 
-chain_with_history = RunnableWithMessageHistory(
+chat_with_history = RunnableWithMessageHistory(
     chain,
     get_history,
     input_messages_key="input",
     history_messages_key="history"
 )
 
-config = {"configurable":{"session_id":"user1"}}
+config = {"configurable":{"session_id": "user_123"}}
 
-print(chain_with_history.invoke({"input": "Hi, my name is Parmeet."}, config=config).content)
-print(chain_with_history.invoke({"input": "What is my name?"}, config=config).content)
+result1 = (chat_with_history.invoke({"input":"Hi My name is Parmeet"}, config=config))
+print(result1.content)
+
+result2 = (chat_with_history.invoke({"input":"Whats my name"}, config=config))
+print(result2.content)
